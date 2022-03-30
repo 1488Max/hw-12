406768d05f22676f24d516329c5e52a2a933d1f7
1488Max <bargamin123@gmail.com>
2022-03-30 16:41:35 +0200

    update of second task

--- SecondTask.java
+++ SecondTask.java
@@ -1,40 +1,81 @@
-public class SecondTask {
-    public static class A extends Thread{
-        public void check(int i) {
-            if(i % 3 == 0 && i%5!=0){
-                System.out.print(" fizz,");
-            }
-        }
-    }
-    public static class B extends Thread{
-        public void check(int i) {
-            if(i % 5 == 0 && i%3!=0){
-                System.out.print(" buzz,");
-            }
-        }
-    }
-    public static class C extends Thread{
-        public void check(int i) {
-            if(i % 5 == 0 && i%3==0){
-                System.out.print(" fizzbuzz,");
-            }
-        }
-    }
-    public static class D extends Thread{
-        public void check(int i) {
-            if(i % 5 != 0 && i%3 != 0){
-                System.out.print(" " +i + ",");
-            }
-        }
-    }
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public static void main(String[] args) {
-        int n = 15;
-        for (int i = 1; i <= n; i++) {
-            new A().check(i);
-            new B().check(i);
-            new C().check(i);
-            new D().check(i);
+public class SecondTask {
+     static boolean isDigit(String string) {
+        char[] chars = string.toCharArray();
+
+        for (char character : chars) {
+            if (Character.isDigit(character)) {
+                return true;
+            }
         }
+
+        return false;
+    }
+    public static void main(String[] args) throws InterruptedException {
+        List<String> strings = new ArrayList(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11",
+                "12", "13", "14", "15"));
+        class A extends Thread {
+            public synchronized void run() {
+                for (int i = 0; i < strings.size(); i++) {
+                    if(isDigit(strings.get(i))) {
+                        if (Integer.parseInt(strings.get(i)) % 3 == 0 && Integer.parseInt(strings.get(i)) % 5 != 0) {
+                            strings.set(i, " fizz");
+                        }
+                    }
+                }
+
+
+            }
+        }
+        class B extends Thread {
+            public synchronized void run()  {
+                for (int i = 0; i < strings.size(); i++) {
+                    if(isDigit(strings.get(i))) {
+                        if (Integer.parseInt(strings.get(i)) % 3 != 0 && Integer.parseInt(strings.get(i)) % 5 == 0) {
+                            strings.set(i, " buzz");
+                        }
+                    }
+                }
+
+
+            }
+        }
+        class C extends Thread {
+            public synchronized void run() {
+                for (int i = 0; i < strings.size(); i++) {
+                    if(isDigit(strings.get(i))) {
+                        if (Integer.parseInt(strings.get(i)) % 3 == 0 && Integer.parseInt(strings.get(i)) % 5 == 0) {
+                            strings.set(i, " fizzbuzz");
+                        }
+                    }
+                }
+
+
+            }
+        }
+        class D extends Thread {
+            public synchronized void run() {
+                for (int i = 0; i < strings.size(); i++) {
+                    if(isDigit(strings.get(i))) {
+                        if (Integer.parseInt(strings.get(i)) % 3 != 0 && Integer.parseInt(strings.get(i)) % 5 != 0) {
+
+                        }
+                    }
+                }
+
+
+            }
+        }
+                new A().start();
+                new B().start();
+                new C().start();
+                new D().start();
+        System.out.println(strings.toString());
+
+
     }
 }
